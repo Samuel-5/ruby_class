6af1585a585c3820a6ac96c8d9bdc70bedb1e451
samuel <samueljim1@gmail.com>
2014-06-25 10:41:19 +0300

    score

---
+++ sam.rb
@@ -0,0 +1,15 @@
+puts "Enter first name"
+firstname = gets
+puts "Enter second name"
+secondname = gets
+puts "Time in"
+Timein = gets.to_i
+#	puts"you are late"
+#elsif(Timein>1000)
+#	puts "come back enkya"
+#	puts "you are suspended for to days"
+		else
+			puts "sign in"
+
+##puts "Time out"
+#puts "phone number"
---
+++ samuel.rb
@@ -0,0 +1,11 @@
+puts "Enter your name"
+name=gets
+puts "Enter Time in"
+time=gets.to_i
+if (time > 8)
+	puts "you are late"
+else
+	puts "sign in"
+	sign_in=gets
+end
+		
